5c1c150f7dd901804326353915b301e3de81ab67
gabrielmoro <gabrielhmoro@gmail.com>
2019-06-03 16:18:15 -0300

    Declarados os semáforos e mais alguma coisa

--- T2/T2.ino
+++ T2/T2.ino
@@ -1,10 +1,14 @@
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h>
 
-#define CHAIR 1
-#define CSTMR 1 /* Customer */
+#define CHAIR 2
+#define CSTMR 2 /* Customer */
 
 const TickType_t dlay = 1000 / portTICK_PERIOD_MS;    /* 1 segundo */
+
+SemaphoreHandle_t semBarber, semCstmrs;
+
+uint8_t freeChairs = CHAIR;
 
 void vPrintStringAndNumber( const char *pcString, unsigned portLONG ulValue ){
   /* Print the string, suspending the scheduler as method of mutual
@@ -26,16 +30,22 @@ void vPrintStringAndNumber( const char *pcString, unsigned portLONG ulValue ){
 }
 
 void barber(void *param){
-
+  for(;;){
+     
+  }
 }
 
 void customer(void *param){
-  
+  for(;;){
+    
+  }
 }
 
 void setup() {
   Serial.begin(9600);
 
+  semBarber = xSemaphoreCreateBinary();
+  semCstmrs = xSemaphoreCreateCounting(CHAIR, 0);
   /* ... */
 
   vTaskStartScheduler();
