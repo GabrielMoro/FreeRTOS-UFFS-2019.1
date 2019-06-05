528d059a73408a84d6885988cf9d8972a37ed81e
gabrielmoro <gabrielhmoro@gmail.com>
2019-06-05 14:16:54 -0300

    Nada ainda

--- T2/T2_Task_Notification/T2_Task_Notification.ino
+++ T2/T2_Task_Notification/T2_Task_Notification.ino
@@ -6,8 +6,44 @@
 
 const TickType_t dlay = 1000 / portTICK_PERIOD_MS;    /* 1 segundo */
 
+TaskHandle_t *barberHandle;
+
+uint8_t freeSeats = SEATS;
+
+void vPrintString( const char *pcString ){
+  vTaskSuspendAll();
+  {
+      Serial.print(pcString);
+      Serial.println("");
+      Serial.flush();
+  }
+  xTaskResumeAll();
+
+  if( Serial.available() )
+  {
+    vTaskEndScheduler();
+  }
+}
+
+void barber(void *param){
+  for(;;){
+    vPrintString("Barber");
+  }
+}
+
+void customer(void *param){
+  for(;;){
+    vPrintString("Customer");
+  }
+}
+
 void setup() {
-  
+  Serial.begin(9600);
+
+  xTaskCreate(barber, NULL, 70, NULL, 1, barberHandle);
+
+  for(uint8_t i = 0; i < CSTMR; i++)
+    xTaskCreate(customer, NULL, 70, NULL, 1, NULL);
 
   vTaskStartScheduler();
   for (;;);
